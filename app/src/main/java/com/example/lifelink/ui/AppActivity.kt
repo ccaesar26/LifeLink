5994e60430b5e78df9823a325c13c33c8c1ad09b
Ivana Florin-Andrei <ivana.florin1987@gmail.com>
2024-11-25 00:18:09 +0200

    Update AppActivity.kt

--- app/src/main/java/com/example/lifelink/ui/AppActivity.kt
+++ app/src/main/java/com/example/lifelink/ui/AppActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.lifelink.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class AppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +20,12 @@ class AppActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_app_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
